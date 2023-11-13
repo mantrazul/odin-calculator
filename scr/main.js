f36b882f4cef90416994c12e1833dcc87cbefaef
mantrazul <aspirina495@gmail.com>
2023-11-12 22:27:33 -0300

    feat: numbers will now appear on screen when clicked, reset button works too

--- scr/main.js
+++ scr/main.js
@@ -1,7 +1,9 @@
 screenHelper = document.querySelector('.screen-helper');
 screenResult= document.querySelector('.result');
+numberButtons = document.querySelectorAll('.number');
+resetButton = document.querySelector('.reset');
 
-window.addEventListener('load', (e) => {
+window.addEventListener('load', () => {
     console.log('loaded');
     clearScreen();
 });
@@ -10,3 +12,13 @@ function clearScreen() {
     screenHelper.innerHTML = '';
     screenResult.innerHTML = '';
 }
+
+function updateScreenHelper() {
+    screenResult.textContent += this.value;
+}
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', updateScreenHelper);
+});
+
+resetButton.addEventListener('click', clearScreen);
