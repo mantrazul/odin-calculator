d93cd076cee900a29876c8cad8e3597d8dcb5cc3
mantrazul <aspirina495@gmail.com>
2023-11-12 22:11:21 -0300

    feat: clear screen text on load

---
+++ scr/main.js
@@ -0,0 +1,12 @@
+screenHelper = document.querySelector('.screen-helper');
+screenResult= document.querySelector('.result');
+
+window.addEventListener('load', (e) => {
+    console.log('loaded');
+    // clearScreen();
+});
+
+function clearScreen() {
+    screenHelper.innerHTML = '';
+    screenResult.innerHTML = '';
+}
