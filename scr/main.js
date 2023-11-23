e42d5a4d3201fe03bea7b889a43b30db244bf87e
mantrazul <aspirina495@gmail.com>
2023-11-22 22:23:44 -0300

    feat: complete the calculator

--- scr/main.js
+++ scr/main.js
@@ -2,11 +2,67 @@ screenHelper = document.querySelector('.screen-helper');
 screenResult= document.querySelector('.result');
 numberButtons = document.querySelectorAll('.number');
 resetButton = document.querySelector('.reset');
+operatorButton = document.querySelectorAll('.operator');
+equalButton = document.querySelector('.equal2');
+pointButton = document.querySelector('.point');
+deleteButton = document.querySelector('.del');
+
+
+resetButton.addEventListener('click', clearScreen);
+
+equalButton.addEventListener('click', equal);
+
+pointButton.addEventListener('click', pointCheck);
+
+deleteButton.addEventListener('click', deleteLast);
+
+operatorButton.forEach(button => {
+    button.addEventListener('click', operation);
+});
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', updateScreenHelper);
+});
 
 window.addEventListener('load', () => {
     console.log('loaded');
     clearScreen();
 });
+
+let operator
+
+document.addEventListener('keydown', (event) => {
+    let key = event.key;
+    if (isFinite(key)) {
+        keyPress(key);
+    }
+    else if (key === '+' || key === '-' || key === 'x' || key === '/') {
+        operator = key;
+        operation();
+    }
+
+    else {
+        switch(key) {
+            case 'Enter':
+                equal();
+                break;
+            case '.':
+                pointCheck();
+                break;
+            case 'Backspace':
+                deleteLast();
+                break;
+
+            case 'c':
+                clearScreen();
+                break;
+        }
+    }
+});
+
+function keyPress(key) {
+    screenResult.textContent += key;
+}
 
 function clearScreen() {
     screenHelper.innerHTML = '';
@@ -14,11 +70,72 @@ function clearScreen() {
 }
 
 function updateScreenHelper() {
-    screenResult.textContent += this.value;
+    if (this.value === '0' && screenResult.innerText === '') {
+        clearBottom();
+    }
+    else {
+        screenResult.innerText += this.value;
+    }
+
 }
 
-numberButtons.forEach(button => {
-    button.addEventListener('click', updateScreenHelper);
-});
+function clearBottom() {
+    screenResult.innerHTML = '';
+}
 
-resetButton.addEventListener('click', clearScreen);
+function operation() {
+    screenHelper.innerText = screenResult.innerText;
+    clearBottom();
+    
+    if (operator !== undefined) {
+        return
+    }
+    else {
+        operator = this.value;
+    }
+}
+
+function equal() {
+    let a = Number(screenHelper.textContent);
+    let b = Number(screenResult.textContent);
+
+    console.log(a, b, operator);
+    if (a ===  0 || b === 0) {
+        return
+    }
+
+    else {
+        let result = 0;
+        switch (operator) {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+            case 'x':
+                result = a * b;
+                break;
+            case '/':
+                result = a / b;
+                break;
+        }
+
+        screenResult.innerText = result;
+        screenHelper.innerText = '';
+        operator = undefined;
+    }
+}
+
+function pointCheck() {
+    if (screenResult.innerText.includes('.')) {
+        return;
+    }
+    else {
+        screenResult.innerText += '.';
+    }
+}
+
+function deleteLast() {
+    screenResult.innerText = screenResult.innerText.slice(0, -1);
+}
